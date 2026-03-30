8999d398878c04157e6e88ed9e260e28f74759d9
canbekcan <can@canbekcan.com>
2026-03-30 07:38:13 +0300

    remove routes.rb

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Discourse::Application.routes.append do
-  post "/user_publications/:username/sync" => "user_publications#trigger_sync"
-  post "/user_publications" => "user_publications#create"
-end
--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,11 @@
 
 enabled_site_setting :user_publications_enabled
 
+Discourse::Application.routes.append do
+  post "/user_publications/:username/sync" => "user_publications#trigger_sync"
+  post "/user_publications" => "user_publications#create"
+end
+
 after_initialize do
   reloadable_patch do
     User.class_eval do
