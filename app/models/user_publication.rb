710502e1a745f3eb691ff5831f1bf336d32d0b71
canbekcan <can@canbekcan.com>
2026-03-30 05:01:23 +0300

    Activate and see profile

--- app/models/user_publication.rb
+++ app/models/user_publication.rb
@@ -3,7 +3,7 @@
 class UserPublication < ActiveRecord::Base
   belongs_to :user
 
-  enum :publication_type, { article: 0, book: 1, chapter: 2, website: 3, proceeding: 4 }
+  enum publication_type: { article: 0, book: 1, chapter: 2, website: 3, proceeding: 4 }
 
   validates :title, presence: true
 end
--- plugin.rb
+++ plugin.rb
@@ -7,24 +7,24 @@
 enabled_site_setting :user_publications_enabled
 
 after_initialize do
-  
-  reloadable_patch do
-    User.class_eval do
-      has_many :user_publications, dependent: :destroy
-    end
+  User.class_eval do
+    has_many :user_publications, dependent: :destroy
   end
 
-  # Inject data securely into the frontend
   add_to_serializer(:user, :publications) do
-    if object.respond_to?(:user_publications)
-      object.user_publications.map do |pub|
-        {
-          id: pub.id,
-          title: pub.title,
-          publication_type: pub.publication_type,
-          url: pub.url
-        }
-      end
+    object.user_publications.map do |pub|
+      {
+        id: pub.id,
+        title: pub.title,
+        publication_type: pub.publication_type,
+        url: pub.url
+      }
     end
+  rescue StandardError
+    []
+  end
+
+  add_to_serializer(:user, :include_publications?) do
+    SiteSetting.user_publications_enabled
   end
 end
